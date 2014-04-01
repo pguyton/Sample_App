ac6af573939565bb558563d860a6360db447ac92
pguyton <pguyton@ussignalcom.com>
2014-04-01 13:58:31 -0400

    Revised_Sample - Cleaned/Revised Sample Application

--- module/Application/src/Application/Model/Customer.php
+++ module/Application/src/Application/Model/Customer.php
@@ -1,6 +1,6 @@
 <?php
-
 namespace Application\Model;
+use Doctrine\Tests\ORM\Functional\EntityRepositoryTest;
 
 /**
  * Class Customer
@@ -22,6 +22,31 @@ class Customer
      * @var string
      */
     protected $description;
+
+    /**
+     * @var string
+     */
+    protected $phone;
+
+    /**
+     * @var string
+     */
+    protected $address;
+
+    /**
+     * @var string
+     */
+    protected $city;
+
+    /**
+     * @var string
+     */
+    protected $state;
+
+    /**
+     * @var string
+     */
+    protected $zip;
 
     /**
      * @param int $id
@@ -69,5 +94,97 @@ class Customer
     public function getDescription()
     {
         return $this->description;
+    }
+
+    /**
+     * @param string $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param string $city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $state
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+    }
+
+    /**
+     * @return string
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param string $zip
+     */
+    public function setZip($zip)
+    {
+        $this->zip = $zip;
+    }
+
+    /**
+     * @return string
+     */
+    public function getZip()
+    {
+        return $this->zip;
+    }
+
+    public function exchangeArray($data)
+    {
+        $this->id     = (isset($data['id']))     ? $data['id']     : null;
+        $this->name = (isset($data['name'])) ? $data['name'] : null;
+        $this->description  = (isset($data['description']))  ? $data['description']  : null;
+        $this->phone    = (isset($data['phone']))     ? $data['phone']     : null;
+        $this->address    = (isset($data['address']))     ? $data['address']     : null;
+        $this->city    = (isset($data['city']))     ? $data['city']     : null;
+        $this->state     = (isset($data['state']))     ? $data['state']     : null;
+        $this->zip     = (isset($data['zip']))     ? $data['zip']     : null;
     }
 }
